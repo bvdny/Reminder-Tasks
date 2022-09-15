4deb4565aa9c24a3d64fc620a0b6c7af6c8b4c66
Viorel <109741144+bvdny@users.noreply.github.com>
2022-09-15 16:19:39 +0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,18 @@
 var cardSignature = 1;
 var countingRemainingTasks = 0;
 function addTask() {
+  var taskType = "listOfTask";
   var task = document.getElementById("task").value;
   var isUrgent = document.querySelector('#urgent:checked') != null;
   var divClass = "card alert alert-light";
   if (isUrgent) {
     divClass = "card alert alert-danger";
+    taskType = "listOfUrgentTask";
   }
   if (task === "") {
     alert("The task description field must not be empty !");
   } else {
-    document.getElementById("listOfTask").innerHTML += `
+    document.getElementById(taskType).innerHTML += `
       <div class="` + divClass + `" id="`+ cardSignature + `">
         <p>Task added on: <strong>`+ getDate() +`</strong> at <strong>`+ getTime() +`</strong></p>
         <div class="card-body">
@@ -45,4 +47,3 @@ function getTime() {
   time = new Date().toLocaleTimeString();
   return time;
 }
-
